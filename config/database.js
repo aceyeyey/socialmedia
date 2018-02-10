9277a20727f2e073362de1bfd83b29fbddc52728
aceyeyey <trainee4@tua.edu.ph>
2018-02-10 12:24:00 +0800

    Updated database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@
 const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
 
 module.exports = {
- uri: 'mongodb://localhost:27017/meandb', // Databse URI and database name
+ // uri: 'mongodb://localhost:27017/meandb', // Databse URI and database name
+ uri:'mongodb://aceyeyey:aceaytin0321@ds227168.mlab.com:27168/meandb',
  secret: crypto, // Cryto-created secret
  db: 'meandb' // Database name
 }
